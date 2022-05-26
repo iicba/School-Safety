bfe37c169928cfb9072b5603f6bd38510541b9c5
iicba <95548021+iicba@users.noreply.github.com>
2022-05-25 20:16:38 -0400

    More Front end JS add

--- public/app.js
+++ public/app.js
@@ -1,36 +1,3 @@
-//console.log('hello world!')
-
-
-
-// function fetchAPI() {
-//     fetch("/api/reports")
-//     .then((res) => res.json())
-//     .then((data) => {
-//         submitters = data;
-//         for (var i=0; i< submitters.length; i++){
-//             var latestSubmittersList = document.createElement("li");
-//             latestSubmittersList.textContent = submitters[i];
-//             ul.appendChild(latestSubmittersList);
-//         }
-//         divUl.appendChild(ul);
-//         document.body.appendChild(divUl);
-//         document.querySelector("#ulContainer").getElementsByClassName.visibility = "visible";
-//     })
-//     .catch((err) => console.log(err));
-// }
-// const body = document.querySelector('body');
-// const btn = document.querySelector('#btn');
-// const list = document.getElementById('destination');
-
-// fetch("http://localhost:3000/api/reports")
-//     .then((res) => res.json())
-//     .then((data) => {
-//         console.log(data)
-//         list.appendChild(data);
-//         body.appendChild(list)
-//     })
-
-
 const body = document.querySelector('body');
 const btn = document.querySelector('#btn');
 const list = document.getElementById('destination');
@@ -38,11 +5,13 @@ const list = document.getElementById('destination');
 addListeners()
 
 function addListeners(){
-    btn.addEventListener('click', getSubmitters)
+    console.log(btn)
+    btn.addEventListener('click', getSubmitters);
+    
 }
 
 async function getSubmitters(){
-    const result = await fetch('https://powerful-brushlands-81845.herokuapp.com/api/reports')
-    const data = await result.json()
-    console.log(data)
+    const result = await fetch('https://powerful-brushlands-81845.herokuapp.com/api/reports');
+    const data = await result.json();
+    console.log(data);
 }
