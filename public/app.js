85b66d80e4c931b6e3f9fffbf57bd3e324e46bbd
iicba <95548021+iicba@users.noreply.github.com>
2022-05-25 20:27:10 -0400

    More Front end JS add

--- public/app.js
+++ public/app.js
@@ -2,12 +2,12 @@ const body = document.querySelector('body');
 const btn = document.getElementById('btn');
 const list = document.getElementById('destination');
 
+
 console.log(btn)
 
 addListeners()
 
 function addListeners(){
-    console.log(btn)
     btn.addEventListener('click', getSubmitters);
     
 }
@@ -15,5 +15,6 @@ function addListeners(){
 async function getSubmitters(){
     const result = await fetch('https://powerful-brushlands-81845.herokuapp.com/api/reports');
     const data = await result.json();
+    list.appendChild(data),
     console.log(data);
 }
