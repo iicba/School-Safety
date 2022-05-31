b34bf54b06f44b2345b94a9ccb858f8ae1a1d859
iicba <95548021+iicba@users.noreply.github.com>
2022-05-31 02:13:16 -0400

    working model

--- public/app.js
+++ public/app.js
@@ -108,7 +108,7 @@ const postText = async (payload) => {
   
   const updateSubmission = async (payload2) => {
     try {
-      const rawResponse = await fetch('/api/reports/:id', {
+      const rawResponse = await fetch(`/api/reports/${payload2.id}`, {
       method: 'PATCH',
       headers: {
         'Accept': 'application/json',
